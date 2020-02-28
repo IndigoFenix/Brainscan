fa02f242c4f1142660f84238e910d6144167a61d
IndigoFenix <indigofenix00@gmail.com>
2020-02-28 17:12:12 +0200

    Update brainscan.lua

--- brainscan.lua
+++ brainscan.lua
@@ -7,12 +7,16 @@ With no parameters, prints out information relating to the selected histfig.
 Adding parameters allows you to get the average personalities of all units in the world.
 Params available:
 {-showall} - Show all values, including average traits.  (Normally only shows traits that are above or below average.)
+{-original} - Uses original personality values (not modified by syndromes) when averaging personalities.
 {-race GOBLIN} - Selects all histfigs of the selected race.  Use the ID from the raws.
 {-prof POET} - Selects all histfigs with the given profession.
 {-cursetag BLOODSUCKER} - Selects all histfigs with the given syndrome tag.
 {-curseclass WERECURSE} - Selects all histfigs with the given syndrome class.
-{-secret} - Selects all histfigs that have a secret interaction.
+{-secret} - Selects all histfigs that have access to at least one secret-type interaction (i.e. necromancers).
 {-book} - Selects all histfigs that have written at least one book.
+{-createdcurse RAISED_UNDEAD} - Selects all histfigs who have created at least one unit with the given syndrome class.
+{-position CUSTOM_LAW_MAKER} - Selects all histfigs that have held the associated position id some point in their life.
+{-responsibility MILITARY_GOALS} - Selects all histfigs that have held a position with the associated responsibility at some point in their life.
 {-hf TYPE NUMBER} - Selects all histfigs with at least the NUMBER relationships of a particular TYPE.
 	hf types are deity,spouse,former_spouse,deceased_spouse,child,lover,mother,father,parent,master,former_master,prisoner,imprisoner,apprentice,former_apprentice
 
@@ -36,6 +40,7 @@ end
 
 local histfig_id = nil
 local streamline = true
+local original_values_only = false
 
 local tierRanges = {{min = 0, max = 9, weight = 4}, {min = 10, max = 24, weight = 20}, {min = 25, max = 39, weight = 85}, {min = 40, max = 60, weight = 780}, {min = 61, max = 75, weight = 85}, {min = 76, max = 90, weight = 20}, {min = 91, max = 100, weight = 4}}
 local tierDesc = {'extremely low','very low','low','average','high','very high','extremely high'}
@@ -178,6 +183,27 @@ function hasPosition(histfig)
 	return nil
 end
 
+function getAllPositions(histfig)
+	local positions = {}
+	for index, link in ipairs(histfig.entity_links) do
+		if (df['histfig_entity_link_former_positionst']:is_instance(link) or df['histfig_entity_link_positionst']:is_instance(link) ) then
+			--printall(df.global.world.entities.all[link.entity_id].positions.own)
+			local position_id = (df.global.world.entities.all[link.entity_id].positions.assignments[link.assignment_id].position_id)
+			local position = nil
+			for i, pos in ipairs(df.global.world.entities.all[link.entity_id].positions.own) do
+				if (pos.id == position_id) then 
+					position = pos
+					break
+				end
+			end
+			if (position ~= nil) then
+				table.insert(positions,position)
+			end
+		end
+	end
+	return positions
+end
+
 function printHistfig(histfig_id)
 	local histfig = df.global.world.history.figures[histfig_id]
 	print (dfhack.TranslateName(histfig.name))
@@ -195,9 +221,21 @@ function printHistfig(histfig_id)
 			for index, traitName in ipairs(df.personality_facet_type) do
 				if (index > -1) then
 					local strength = histfig.info.personality.anon_1.traits[traitName]
+					local change = 0
+					if (histfig.info.personality.anon_1.temporary_trait_changes ~= nil) then
+						if (histfig.info.personality.anon_1.temporary_trait_changes[index] ~= 0) then
+							change = histfig.info.personality.anon_1.temporary_trait_changes[index]
+							strength = strength + change
+						end
+					end
 					local tier = getTraitTier(strength)
-					if (streamline == false or tier ~= 4) then
-						print (traitName .. ': ' .. tierDesc[tier] .. ' (' .. strength .. ')')
+					if (streamline == false or tier ~= 4 or change ~= 0) then
+						local changestr = ''
+						if change ~= 0 then
+							if change > 0 then changestr = ' (+' .. change .. ')'
+							else changestr = ' (' .. change .. ')' end
+						end
+						print (traitName .. ': ' .. tierDesc[tier] .. ' (' .. strength .. ')' .. changestr)
 					end
 				end
 			end
@@ -239,6 +277,11 @@ function printHistfig(histfig_id)
 			end
 		end
 		
+		local allpositions = getAllPositions(histfig)
+		
+		for index, pos in pairs(allpositions) do
+			--printall(pos.responsibilities)
+		end
 		
 		if (histfig.info.kills ~= nil) then
 		end
@@ -251,15 +294,23 @@ function printHistfig(histfig_id)
 			--print ('Link type: ' .. getHfLinkType(link))
 		end
 	end
+	--18479
+	--printall(histfig.info.pets) animals that have been tamed
+	--printall(df.global.world.nemesis.all[10].figure)
+	--printall(df.global.world.raws.creatures.all[335])
 	
 		--printall (df.global.world.nemesis.all[unit_id])
 end
 
+--histfig_id = 301
+
 function getAverages(params)
 	local allfigs = df.global.world.history.figures
-	local figcount = 0
+	local personality_count = 0
+	local professionals_count = 0
 	local trait_totals = {}
 	local value_totals = {}
+	local skill_totals = {}
 	local i
 	local hf
 	for index, traitName in ipairs(df.personality_facet_type) do
@@ -308,6 +359,24 @@ function getAverages(params)
 				if (syndrome == nil or syndrome[params['synclass']] == nil) then valid = false end
 			end
 		end
+		if (params['created_curse'] ~= nil) then
+			if (histfig.info == nil or histfig.info.relationships == nil) then 
+				valid = false 
+			else
+				local foundone = false
+				for index, rel in ipairs(histfig.info.relationships.hf_visual) do
+					local fig = df.global.world.history.figures[rel.histfig_id]
+					if (fig.info ~= nil and fig.info.curse ~= nil) then
+						local syndrome = synClasses(fig.info.curse)
+						if (syndrome ~= nil and syndrome[params['created_curse']] ~= nil) then 
+							foundone = true 
+							break
+						end
+					end
+				end
+				if (foundone == false) then valid = false end
+			end
+		end
 		if (params['hf'] ~= nil) then
 			local hf_count = tonumber(params['hf_count'])
 			local counted = 0
@@ -324,7 +393,33 @@ function getAverages(params)
 		if (params['secret'] ~= nil and secretClasses(histfig) == nil) then valid = false end
 		if (params['book'] ~= nil and (histfig.info == nil or histfig.info.books == nil)) then valid = false end
 		
-		if (params['position'] ~= nil and (hasPosition(histfig) == nil)) then valid = false end
+		local allpositions = {}
+		
+		if (params['position'] ~= nil or params['responsibility'] ~= nil) then
+			allpositions = getAllPositions(histfig)
+		end
+		
+		if (params['position'] ~= nil) then
+			--if (hasPosition(histfig) == nil) then valid = false end
+			local found = false
+			for index, pos in pairs(allpositions) do
+				if pos.code == params['position'] then 
+					found = true
+					break
+				end
+			end
+			if (found == false) then valid = false end
+		end
+		if (params['responsibility'] ~= nil) then
+			local found = false
+			for index, pos in pairs(allpositions) do
+				if (pos.responsibilities[params['responsibility']] == true) then 
+					found = true
+					break
+				end
+			end
+			if (found == false) then valid = false end
+		end
 		
 		if valid == true then
 			if (histfig.info ~= nil) then
@@ -332,6 +427,12 @@ function getAverages(params)
 					for index, traitName in ipairs(df.personality_facet_type) do
 						if (index > -1) then
 							local strength = histfig.info.personality.anon_1.traits[traitName]
+							if (original_values_only == false and histfig.info.personality.anon_1.temporary_trait_changes ~= nil) then
+								if (histfig.info.personality.anon_1.temporary_trait_changes[index] ~= 0) then
+									local change = histfig.info.personality.anon_1.temporary_trait_changes[index]
+									strength = strength + change
+								end
+							end
 							trait_totals[traitName] = trait_totals[traitName] + strength
 						end
 					end
@@ -341,23 +442,37 @@ function getAverages(params)
 						if value_totals[value_type] == nil then value_totals[value_type] = 0 end
 						value_totals[value_type] = value_totals[value_type] + value.strength
 					end
-					figcount = figcount + 1
+					personality_count = personality_count + 1
+				end
+				
+				if (histfig.info.skills ~= nil and streamline == false) then
+					for i=1, #histfig.info.skills.skills do
+						local skill_type = df.job_skill[histfig.info.skills.skills[i-1]]
+						if skill_totals[skill_type] == nil then skill_totals[skill_type] = 0 end
+						skill_totals[skill_type] = skill_totals[skill_type] + histfig.info.skills.points[i-1]
+					end
+					professionals_count = professionals_count + 1
 				end
 			end
 		end
 		
 	end
-	print (figcount .. ' figures scanned')
-	if (figcount > 0) then
+	print (personality_count .. ' personalities scanned')
+	if (streamline == false) then print (professionals_count .. ' professionals scanned') end
+	if (personality_count > 0) then
 		local trait_averages = {}
 		local value_averages = {}
+		local skill_averages = {}
 		for index, traitName in pairs(trait_totals) do
 			if (index ~= 'NONE') then
-				trait_averages[index] = trait_totals[index] / figcount
+				trait_averages[index] = trait_totals[index] / personality_count
 			end
 		end
 		for index, traitName in pairs(value_totals) do
-			value_averages[index] = value_totals[index] / figcount
+			value_averages[index] = value_totals[index] / personality_count
+		end
+		for index, traitName in pairs(skill_totals) do
+			skill_averages[index] = skill_totals[index] / professionals_count
 		end
 		local has_outlier = false
 		for traitName, strength in pairs(trait_averages) do
@@ -377,12 +492,20 @@ function getAverages(params)
 			end
 		end
 		if (has_outlier == false) then
-			print('No statistically significant trends. Use -showall to view all averages.')
+			print('No statistically significant personality or value trends. Use -showall to view all averages.')
+		end
+		if (streamline == false) then
+			print ('Skills:')
+			for valueName, strength in pairs(skill_averages) do
+				--local strength = trait_averages[traitName]
+				if (streamline == false) then
+					print (valueName .. ': ' .. strength)
+				end
+			end
 		end
 		--printall(trait_averages)
 	end
 end
-
 
 if #args == 0 or (#args == 1 and args[1] == '-showall') then
 	if (#args == 1) then streamline = false end
@@ -416,6 +539,8 @@ else
 			end
 		elseif (arg == '-showall') then
 			streamline = false
+		elseif (arg == '-original') then
+			original_values_only = true
 		elseif (arg == '-prof') then
 			params['profession'] = args[a+1]
 		elseif (arg == '-vampire') then
@@ -431,8 +556,12 @@ else
 			params['hf_count'] = args[a+2]
 		elseif (arg == '-book') then
 			params['book'] = true
+		elseif (arg == '-createdcurse') then
+			params['created_curse'] = args[a+1]
 		elseif (arg == '-position') then
-			params['position'] = true
+			params['position'] = args[a+1]
+		elseif (arg == '-responsibility') then
+			params['responsibility'] = args[a+1]
 		elseif (arg == '-battle_leader') then
 			params['battle_leader'] = true
 		elseif (arg == '-secret') then
